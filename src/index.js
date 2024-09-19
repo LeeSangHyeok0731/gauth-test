71fb033b0ea6011de6042d42980c7e933fd11250
fakesanghyeok <s24066@gsm.hs.kr>
2024-09-19 23:16:22 +0900

    수정3

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import MyComponent from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>
-      <h1>gauth-test용 입니다</h1>
-    </div>
-    <MyComponent />
+    <BrowserRouter>
+      <div>
+        <h1>gauth-test용 입니다</h1>
+      </div>
+      <MyComponent />
+    </BrowserRouter>
   </React.StrictMode>
 );
-
